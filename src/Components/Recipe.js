6af704b087b597f04c7cd37462fc948f4d52d7a5
kirandesimone <kirandesimone@gmail.com>
2020-12-06 11:25:04 -0500

    finish mobile individual recipe design

--- src/Components/Recipe.js
+++ src/Components/Recipe.js
@@ -1,7 +1,8 @@
-import { Typography } from '@material-ui/core';
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import useMediaQuery from '@material-ui/core/useMediaQuery'
 import recipesData from '../Data/recipesData.json'
+import { Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles'
 import theme from '../theme'
 
@@ -12,9 +13,27 @@ const useStyles = makeStyles({
         paddingTop: "5%",
         paddingBottom: "10%",
     },
+    mblMain: {
+        backgroundColor: theme.palette.primary.main,
+        display: "flex",
+        flexDirection: "column",
+        paddingTop: "5%",
+        paddingBottom: "10%",
+    },
     dish: {
         paddingTop: "2%",
         paddingLeft: "5%"
+    },
+    mblDish: {
+        paddingTop: "2%",
+        paddingLeft: "3%"
+    },
+    dishName: {
+        fontFamily: theme.typography.fontFamily
+    },
+    mblDishName: {
+        fontFamily: theme.typography.fontFamily,
+        fontSize: "1.85rem"
     },
     picture: {
         paddingTop: "5%",
@@ -23,6 +42,11 @@ const useStyles = makeStyles({
         paddingTop: "7%",
         paddingLeft: "10%",
         maxWidth: 700
+    },
+    mblContent: {
+        paddingTop: "7%",
+        paddingLeft: "3%",
+        maxWidth: 500
     },
     directions: {
         paddingBottom: "4%"
@@ -34,17 +58,23 @@ export default function Recipe() {
     const recipeId = parseInt(id);
     const recipe = recipesData.filter(data => recipeId === data.id).pop();
     const classes = useStyles();
+    const matches = useMediaQuery('(max-width:600px)');
+
     return (
-        <div className={classes.main}>
-            <div className={classes.dish}>
-                <Typography variant="h3" style={{fontFamily: theme.typography.fontFamily}}>
+        <div className={matches ? classes.mblMain : classes.main}>
+            <div className={matches ? classes.mblDish : classes.dish}>
+                <Typography variant="h3" className={matches ? classes.mblDishName : classes.dishName}>
                     { recipe.recipe.dishName }
                 </Typography>
                 <div className={classes.picture}>
-                    <img src={require(`../Assets/${recipe.recipe.picture}`)} alt={recipe.recipe.dishName}/>
+                    <img src={ matches ? 
+                    require(`../Assets/${recipe.recipe.mbl_pic}`) : 
+                    require(`../Assets/${recipe.recipe.picture}`)} 
+                    alt={recipe.recipe.dishName}
+                    />
                 </div>
             </div>
-            <div className={classes.content}>
+            <div className={matches ? classes.mblContent : classes.content}>
                 <div className={classes.directions}>
                     <Typography variant="h5" style={{paddingBottom: "2%", fontFamily: theme.typography.fontFamily}}>
                         Directions:
