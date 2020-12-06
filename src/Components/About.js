fbf5d7d61ebbb00843fb768ab8a1c6b314d625c4
kirandesimone <kirandesimone@gmail.com>
2020-12-06 11:10:11 -0500

    finish mobile design for about and recipe page

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,24 +1,42 @@
 import React from 'react'
 import theme from '../theme'
+import useMediaQuery from '@material-ui/core/useMediaQuery'
 import { makeStyles } from '@material-ui/core/styles'
 import Typography  from '@material-ui/core/Typography'
 import aboutPic from '../Assets/rsz_about.jpg'
+import mblAboutPic from '../Assets/mbl_about.jpg'
 
 
 const useStyles = makeStyles({
     about: {
+        display: "flex",
         backgroundColor: theme.palette.primary.dark,
         paddingTop: "10%",
         paddingBottom: "5%",
-        display: "flex"
+    },
+    mblAbout: {
+        display: "flex",
+        flexDirection: "column",
+        backgroundColor: theme.palette.primary.dark,
+        paddingTop: "10%",
+        paddingBottom: "5%",
     },
     imgWrapper: {
         marginLeft: "5%",
         overflow: "hidden",
         paddingRight: "8%"
     },
+    mblImgWrapper: {
+        marginLeft: "3%",
+        overflow: "hidden",
+    },
     contentWrapper: {
         maxWidth: 700,
+        paddingTop: "10%"
+    },
+    mblContentWrapper: {
+        maxWidth: 600,
+        marginLeft: "2.5%",
         paddingTop: "10%"
     },
     title: {
@@ -26,21 +44,28 @@ const useStyles = makeStyles({
         color: theme.palette.primary.main,
         fontFamily: theme.typography.fontFamily
     },
+    mblTitle: {
+        paddingBottom: "5%",
+        color: theme.palette.primary.main,
+        fontFamily: theme.typography.fontFamily,
+        fontSize: "2.2rem"
+    }
     
 });
 
 export default function About() {
     const classes = useStyles();
+    const matches = useMediaQuery('(max-width:600px)');
 
     return (
         <div>
-            <section className={classes.about}>
-                <div className={classes.imgWrapper}>
-                    <img src={aboutPic} alt="food"/>
+            <section className={matches ? classes.mblAbout : classes.about}>
+                <div className={matches ? classes.mblImgWrapper : classes.imgWrapper}>
+                    <img src={matches? mblAboutPic : aboutPic} alt="food"/>
                 </div>
                 <div style={{display: "flex", flexDirection: "column"}}>
-                    <div className={classes.contentWrapper}>
-                        <Typography variant="h2" className={classes.title}>
+                    <div className={matches ? classes.mblContentWrapper : classes.contentWrapper}>
+                        <Typography variant="h2" className={matches ? classes.mblTitle : classes.title}>
                             About...
                         </Typography>
                         <Typography variant="body1" style={{fontSize: "1.2rem"}}>
@@ -49,8 +74,8 @@ export default function About() {
                             always with an alkaline plant-based approach.
                         </Typography>
                     </div>
-                    <div className={classes.contentWrapper}>
-                        <Typography variant="h3" className={classes.title}>
+                    <div className={matches ? classes.mblContentWrapper : classes.contentWrapper}>
+                        <Typography variant="h3" className={matches ? classes.mblTitle : classes.title}>
                             The Author
                         </Typography>
                         <Typography variant="body1" style={{fontSize:"1.2rem"}}>
--- src/Components/Recipes.js
+++ src/Components/Recipes.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import RecipeCards from './RecipeCards'
 import Typography from '@material-ui/core/Typography'
+import useMediaQuery from '@material-ui/core/useMediaQuery'
 import recipesData from '../Data/recipesData.json'
 import { makeStyles } from '@material-ui/core/styles'
 import theme from '../theme'
@@ -13,11 +14,20 @@ const useStyles = makeStyles({
     },
     headerWrapper: {
         paddingBottom: "10%",
-        paddingLeft: "42%"
+        marginLeft: "42%"
+    },
+    mblHeaderWrapper: {
+        paddingBottom: "10%",
+        marginLeft: "27%"
     },
     header: {
         color: theme.palette.primary.main,
         fontFamily: theme.typography.fontFamily
+    },
+    mblHeader: {
+        color: theme.palette.primary.main,
+        fontFamily: theme.typography.fontFamily,
+        fontSize: "3rem"
     },
     recipeContainer: {
         display: "flex",
@@ -25,21 +35,30 @@ const useStyles = makeStyles({
         justifyContent: "space-between",
         paddingLeft: "10%",
         paddingRight: "10%",
+    },
+    mblRecipeContainer: {
+        marginLeft: '3.75%',
+    },
+    mblRecipeSpace: {
+        paddingBottom: "5%"
     }
+
 })
 
 export default function Recipes() {
     const classes = useStyles();
+    const matches = useMediaQuery('(max-width:600px)');
+
     return (
         <div className={classes.main}>
-            <div className={classes.headerWrapper}>
-                <Typography variant="h1" className={classes.header}>
+            <div className={matches ? classes.mblHeaderWrapper : classes.headerWrapper}>
+                <Typography variant="h1" className={matches ? classes.mblHeader : classes.header}>
                     Recipes
                 </Typography>
             </div>
-            <div className={classes.recipeContainer}>
+            <div className={matches ? classes.mblRecipeContainer : classes.recipeContainer}>
                 { recipesData.map(recipe =>
-                <div key={recipe.id}>
+                <div key={recipe.id} className={matches ? classes.mblRecipeSpace : " "}>
                     <RecipeCards { ...recipe } />
                 </div>
                 )}
